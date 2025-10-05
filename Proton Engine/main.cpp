6e4070891265077bc02be90ec40a0e0a749f6c89
Chiller Cat <81083840+Eternal587@users.noreply.github.com>
2025-10-05 09:16:50 -0400

    inital Commit, Wiring Up Dependicies
    OpenGL, and Glut have been linked

---
+++ Proton Engine/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Proton Engine
+//
+//  Created by VibingCatt on 10/5/25.
+//
+
+#include <iostream>
+#include <OpenGL/gl.h>
+#include <GLUT/glut.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
